8d7ec31a9e8340e91943eb75d6c82405a256bf5f
Vasco Saraiva <vasco.saraiva@ua.pt>
2023-12-19 16:07:24 +0000

    Terceira tentativa

--- app/page.jsx
+++ app/page.jsx
@@ -2,9 +2,9 @@
 export default function Home() {
 
   return (
-    <h1 className="bg-primary_main hover:bg-primary_dark text-white">
+    <main className="bg-primary_main hover:bg-primary_dark text-white">
       Olá
-    </h1>
+    </main>
   );
 }
 

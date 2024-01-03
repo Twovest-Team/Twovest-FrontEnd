d19bb4d40febe7558cd8e885420124921089bafe
jkapa17 <tomaspaixao2002@gmail.com>
2024-01-03 12:09:38 +0000

    Quarta versão do footer (Incompleto)

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,8 @@
 import Image from "next/image"
 import LogoWhite from '@/public/images/logo_twovest_white.svg';
 import SocialMediaLogos from "./SocialMediaLogos";
+import PaymentLogos from "./PaymentLogos";
+import LanguageButton from "./LanguageButton";
 
 export default function Footer () {
     return(
@@ -13,20 +15,30 @@ export default function Footer () {
             />
             </div>
             <div className="flex flex-wrap justify-center text-white">
-            <div className="texto_footer w-40 pr-4 border-r white_opacity_50">Contactos</div>
-            <div className="texto_footer w-40 pl-4">Proteção de dados</div>
-            <div className="texto_footer w-40 pr-4 border-r white_opacity_50">Help center</div>
-            <div className="texto_footer w-40 pl-4">Termos de serviço</div>
-            <div className="texto_footer w-40 pr-4 border-r white_opacity_50">Sobre nós</div>
-            <div className="texto_footer w-40 pl-4">Aviso legal</div>
+            <div className="footer--text w-40 pr-4 border-r white_opacity_50">Contactos</div>
+            <div className="footer--text w-40 pl-4">Proteção de dados</div>
+            <div className="footer--text w-40 pr-4 border-r white_opacity_50">Help center</div>
+            <div className="footer--text w-40 pl-4">Termos de serviço</div>
+            <div className="footer--text w-40 pr-4 border-r white_opacity_50">Sobre nós</div>
+            <div className="footer--text w-40 pl-4">Aviso legal</div>
             </div>
             <div className="mt-8 mb-6">
             <SocialMediaLogos />
             </div>
             <hr className="mx-6 mb-6"/>
-            <div className="text-white">
-            Espaço para seleção de linguagem e trademark twovest
+            <div className="flex flex-wrap justify-left mx-6 text-white">
+            <div className="mr-40">
+            <LanguageButton />
             </div>
+            <div>
+            @ 2023 twovest
+            </div>
+            </div>
+            
+            <div className="bg-white py-3.5 mt-5">
+            <PaymentLogos />
+            </div>
+
         </footer>
     )
 }

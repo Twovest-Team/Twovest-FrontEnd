caf8cf56a9ac7875c6ed888f865aa4f01a808221
gcalcadaMCTW <147869584+gcalcadaMCTW@users.noreply.github.com>
2024-01-03 15:44:15 +0000

    Revert "Button Componoments added"

--- app/page.jsx
+++ app/page.jsx
@@ -1,29 +1,11 @@
 import { markAssetError } from "next/dist/client/route-loader";
 
-import  {Buttons}  from "../components/Buttons";
-
 export default function Home() {
 
   return (
-
-    <main>
-
-  <div className="flex gap-4">
-    <Buttons btnState="defaultMain" text="Sustentável" icon="navigateNext" btnSize="menuSize" />
-    <Buttons btnState="defaultMain" text="Promoções" icon="offer" btnSize="menuSize" />
-    
-</div>
-
-      <Buttons aria-label="Next" btnState="defaultMain" text="Proximo" icon="navigateNext" btnSize="mediumSize"/>
-      <Buttons btnState="secondaryMain" text="Registar Conta" btnSize="mediumSize"/>
-      <Buttons btnState="secondaryMain" text="Continuar com Google" btnSize="mediumSizeSocials" icon="google"/>
-      <Buttons btnState="secondaryMain" text="Continuar com Facebook" btnSize="mediumSizeSocials" icon="facebook"/>
-      <Buttons btnState="secondaryMain" text="Continuar com Maça" btnSize="mediumSizeSocials" icon="apple"/>
-      <Buttons btnState="secondaryMain" text="Redefinir" btnSize="mediumSizeSocials" icon="redifine"/>
-      <Buttons btnState="defaultMain" text="Voltar a loja" btnSize="mediumSizeSocials"/>
+    <main className="bg-primary_main hover:bg-primary_dark text-white">
+      Olá
     </main>
-    
-   
   );
   
 }

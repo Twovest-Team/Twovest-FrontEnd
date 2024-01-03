709d7609f93db93ac647750f1e813bb4fadf6c48
gcalcada <gcalcada7@ua.pt>
2024-01-03 12:55:38 +0000

    Button Componoments updated hover buttons, aria text included, new icons

--- app/page.jsx
+++ app/page.jsx
@@ -9,15 +9,17 @@ export default function Home() {
 
   <div className="flex gap-4">
     <Buttons btnState="defaultMain" text="Sustentável" icon="navigateNext" btnSize="menuSize" />
-    <Buttons btnState="defaultMain" text="Promoções" icon="navigateNext" btnSize="menuSize" />
+    <Buttons btnState="defaultMain" text="Promoções" icon="offer" btnSize="menuSize" />
     
 </div>
 
-      <Buttons btnState="defaultMain" text="Proximo" icon="navigateNext" btnSize="mediumSize"/>
+      <Buttons aria-label="Next" btnState="defaultMain" text="Proximo" icon="navigateNext" btnSize="mediumSize"/>
       <Buttons btnState="secondaryMain" text="Registar Conta" btnSize="mediumSize"/>
       <Buttons btnState="secondaryMain" text="Continuar com Google" btnSize="mediumSizeSocials" icon="google"/>
       <Buttons btnState="secondaryMain" text="Continuar com Facebook" btnSize="mediumSizeSocials" icon="facebook"/>
       <Buttons btnState="secondaryMain" text="Continuar com Maça" btnSize="mediumSizeSocials" icon="apple"/>
+      <Buttons btnState="secondaryMain" text="Redefinir" btnSize="mediumSizeSocials" icon="redifine"/>
+      <Buttons btnState="defaultMain" text="Voltar a loja" btnSize="mediumSizeSocials"/>
     </main>
     
    

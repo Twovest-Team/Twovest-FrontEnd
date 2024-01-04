e0c307c30c542d09939640f30bbd86860b7ab7e9
Vasco Saraiva <vasco.saraiva@ua.pt>
2024-01-04 15:53:58 +0000

    Criei utility function handleGender.js

--- app/landing/page.jsx
+++ app/landing/page.jsx
@@ -3,8 +3,8 @@
 import Image from "next/image";
 import ModeloHomem from "@/public/images/landing_page/landing_homem.png";
 import ModeloMulher from "@/public/images/landing_page/landing_mulher.png";
-import setLocalStorage from "@/utils/localStorage/setLocalStorage";
 import { useRouter } from "next/navigation";
+import handleGender from "@/utils/handleGender";
 
 // Se utilizador já tem um género selecionado deve ser redirecionado para a homepage
 export default function Landing() {
@@ -16,18 +16,17 @@ export default function Landing() {
     { gender: 'homem', image: ModeloHomem },
   ]
 
-  function handleGender(gender){
-    setLocalStorage('gender', gender)
+  function handleClick(gender){
+    handleGender(gender)
     router.push('/')
   }
-
-
+  
   return (
     <div className="container">
       <div className="flex justify-center flex-wrap my-8 gap-8">
 
         {categories.map(element => (
-          <button onClick={() => handleGender(element.gender)} className="relative">
+          <button onClick={() => handleClick(element.gender)} className="relative">
             <Image
               src={element.image}
               alt="Modelo feminino da Twovest"
@@ -38,7 +37,6 @@ export default function Landing() {
             <h3 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white font-semibold capitalize">{element.gender}</h3>
           </button>
         ))}
-
 
       </div>
     </div>

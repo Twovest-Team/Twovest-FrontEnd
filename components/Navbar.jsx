a2640ca51935ff02a87159af2ba936e7f44e65b7
Jónatas Roque <jonatasroque@ua.pt>
2024-01-04 12:02:19 +0000

    componentes para categorias do menu

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -15,6 +15,11 @@ import calcado from "../public/images/categories/calcado.png";
 import acessorios from "../public/images/categories/acessorios.png";
 import sustentavel from "../public/images/icons/sustentavel.svg";
 
+//import componentes pags
+import { VestuarioLista } from "./menuCategories/Vestuario";
+import { CalcadoLista } from "./menuCategories/Calcado";
+import { AcessoriosLista } from "./menuCategories/Acessorios";
+
 //import de icons materialUI
 import MenuIcon from '@mui/icons-material/Menu';
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
@@ -204,64 +209,17 @@ export const Navbar = () =>{
         </div>
 
         {nameCategory == "Vestuário" && (
-            <div>
-            <ul className="my-6 mx-6">
-            <h6 className="font-semibold">Partes de cima</h6>
-            <p className="my-3">Camisolas</p>
-            <p className="my-3">Malhas</p>
-            <p className="my-3">Sweatshirts</p>
-            <p className="my-3">T-shirts</p>
-            <p className="my-3">Polos</p>
-            <p className="my-3">Casacos</p>
-            <p className="my-3">Blazers</p>
-            <p className="my-3">Coletes</p>
-            <p className="my-3">Camisas</p>
-            <p className="my-3">Sobretudos</p>
-        </ul>
-
-        <ul className="my-6 mx-6">
-            <h6 className="font-semibold">Partes de baixo</h6>
-            <p className="my-3">Calças</p>
-            <p className="my-3">Calções</p>
-        </ul>
-
-        <ul className="my-6 mx-6">
-            <h6 className="font-semibold">Corpo inteiro</h6>
-            <p className="my-3">Macacões</p>
-        </ul>
-        </div>
+            <VestuarioLista/>
         )}
         
         
         {nameCategory == "Calçado" && (
-            <ul className="my-6 mx-6">
-            <h6 className="font-semibold">Calçado</h6>
-            <p className="my-3">Sapatilhas/Ténis</p>
-            <p className="my-3">Sapatos</p>
-            <p className="my-3">Botas</p>
-            <p className="my-3">Sandálias</p>
-        </ul>
+            <CalcadoLista/>
         )}
 
                 
         {nameCategory == "Acessórios" && (
-            <ul className="my-6 mx-6">
-                <h6 className="font-semibold">Acessórios</h6>
-                <p className="my-3">Chapéus</p>
-                <p className="my-3">Cintos</p>
-                <p className="my-3">Óculos</p>
-                <p className="my-3">Pulseiras</p>
-                <p className="my-3">Colares</p>
-                <p className="my-3">Anéis</p>
-                <p className="my-3">Cachecóis</p>
-                <p className="my-3">Gorros</p>
-                <p className="my-3">Golas</p>
-                <p className="my-3">Luvas</p>
-                <p className="my-3">Brincos</p>
-                <p className="my-3">Carteiras</p>
-                <p className="my-3">Malas</p>
-                <p className="my-3">Mochilas</p>
-            </ul>
+            <AcessoriosLista/>
         )}
         
 

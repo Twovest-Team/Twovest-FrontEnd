6388be9c7256b7bf33df765103b826333666f5c4
Vasco Saraiva <vasco.saraiva@ua.pt>
2024-01-04 13:14:30 +0000

    Adicionei rotas que faltavam

---
+++ app/profile/collections/[collection]/page.jsx
@@ -0,0 +1,12 @@
+
+// Coleção específica de um utilizador
+const Collection = () => {
+
+    // Deverá ser necessário efetuar um pedido à BD "Dá os dados da collection com o id=123"
+
+  return (
+    <div>Collection</div>
+  )
+}
+
+export default Collection
--- app/profile/collections/page.jsx
+++ app/profile/collections/page.jsx
@@ -1,3 +1,7 @@
+
+//Lista com todas coleções associada a um perfil (incluindo o meu próprio perfil)
+// Se for coleções de outros utilizadores, apenas mostrar as coleções públicas e/ou coleções que esse utilizador partilha comigo
+// Se for as minhas coleções, mostrar privadas, públicas e partilhadas (mesmo que não seja o dono de uma coleção partilhada)
 const Collections = () => {
   return (
     <div>Collections</div>

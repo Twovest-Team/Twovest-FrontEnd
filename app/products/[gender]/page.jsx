d6c471d14ccf2b0afad483db6ea9264f38dc9bd4
Vasco Saraiva <vasco.saraiva@ua.pt>
2024-01-04 20:39:07 +0000

    Ligeira alteração nas rotas

--- app/products/[gender]/page.jsx
+++ app/products/[gender]/page.jsx
@@ -1,13 +1,31 @@
 
+import CardProduct from "@/components/CardProduct"
+import getProductsByCategory from "@/utils/db/getProductsByCategory"
+import Views from "@/components/Views"
+import ItemsBox from "@/components/ItemsBox"
+
 // Página com todos os produtos, filtrados por categoria.
 // Exemplo: twovest.com/products/mulher?category='Saias'
 // Atenção, carregar 30 produtos de cada vez (por exemplo), infinite scroll
-const Products = () => {
 
- 
+export const revalidate = 0 
+
+const Products = async () => {
+
+  const data = await getProductsByCategory(11, 'Mulher')
 
   return (
-    <div>Products</div>
+    <main>
+      <div className="container flex justify-between">
+      <Views />
+      </div>
+      
+      <ItemsBox>
+        {data.map(element => <CardProduct product={element} />)}
+      </ItemsBox>
+    </main>
+
+
   )
 }
 

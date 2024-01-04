b2a34f60229bcd0d3f1d0d04d63bea801f39e56e
gcalcada <gcalcada7@ua.pt>
2024-01-04 10:45:07 +0000

    Routing

---
+++ app/galeria/page.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+export default function About() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>maravilha</p>
+    </div>
+  );
+}
+
--- app/page.jsx
+++ app/page.jsx
@@ -1,28 +1,16 @@
-
-import  {Buttons}  from "../components/Buttons";
+import React from 'react';
+import Link from 'next/link';
+import { Buttons } from '@/components/Buttons';
 
 export default function Home() {
-
   return (
-    <main>
-
-    <div className="flex gap-4">
-      <Buttons btnState="defaultMain" text="Sustentável" icon="navigateNext" btnSize="menuSize" />
-      <Buttons btnState="defaultMain" text="Promoções" icon="offer" btnSize="menuSize" />
-  
-  </div>
-  
-        <Buttons aria-label="Next" btnState="defaultMain" text="Proximo" icon="navigateNext" btnSize="mediumSize"/>
-        <Buttons btnState="secondaryMain" text="Registar Conta" btnSize="mediumSize"/>
-        <Buttons btnState="secondaryMain" text="Continuar com Google" btnSize="mediumSizeSocials" icon="google"/>
-        <Buttons btnState="secondaryMain" text="Continuar com Facebook" btnSize="mediumSizeSocials" icon="facebook"/>
-        <Buttons btnState="secondaryMain" text="Continuar com Maça" btnSize="mediumSizeSocials" icon="apple"/>
-        <Buttons btnState="secondaryMain" text="Redefinir" btnSize="mediumSizeSocials" icon="redifine"/>
-        <Buttons btnState="defaultMain" text="Voltar a loja" btnSize="mediumSize"/>
-      </main>
-    
-   
+    <div>
+      <p>Olá! Esta é a página inicial.</p>
+      <Link href="/galeria">
+      <Buttons btnState="defaultMain" text="hello" btnSize="mediumSize" >
+      
+      </Buttons>
+      </Link>
+    </div>
   );
-  
 }
-

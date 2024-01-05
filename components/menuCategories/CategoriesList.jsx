dffe66cd953cbd7d446235e52fa504abca06ec60
Jónatas Roque <jonatasroque@ua.pt>
2024-01-05 19:13:19 +0000

    correção de key props no menu categorias

--- components/menuCategories/CategoriesList.jsx
+++ components/menuCategories/CategoriesList.jsx
@@ -1,47 +1,44 @@
-import { main_categories, categories } from "@/constants"
-import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { main_categories, categories } from "@/constants";
+import { useRouter } from "next/navigation"; 
 
-export const CategoriesList = ({idCategory, genderState, toggleMenu, toggleCategory}) =>{
+export const CategoriesList = ({ idCategory, genderState, toggleMenu, toggleCategory }) => {
+  let gender;
+  const router = useRouter();
 
-    let gender;
+  const handleClickCategory = (category) => {
+    toggleMenu();
+    toggleCategory();
+    router.push(`/products/${genderState}?category=${category}`);
+  };
 
-    const router = useRouter();
+  if (genderState == "mulher") {
+    gender = 1;
+  } else {
+    gender = 2;
+  }
 
-    const handleClickCategory = (category) =>{
-        toggleMenu();
-        toggleCategory();
-        router.push(`/products/${genderState}?category=${category}`)
-    }
-
-    if(genderState == "mulher"){
-        gender=1;
-    }else{
-        gender=2;
-    }
-
-    return(
-        <ul className="my-6 mx-6">
-            
-            {main_categories.map((e) =>
-            <div key={e.id}>
-            
-            {idCategory == e.general_category && 
-
-            <div className="my-6" >
-                <h6 className="font-semibold">{e.plural}</h6>
-            
-            {categories.map((category) => 
-            <>
-            {category.main_category == e.id && category.gender.includes(gender) &&
-
-             <button className="my-4 block" onClick={()=> handleClickCategory(category.plural)}>{category.plural}</button>}
-            </>)}
-            
-            </div>}
-
+  return (
+    <ul className="my-6 mx-6">
+      {main_categories.map((e) => (
+        <div key={e.id}>
+          {idCategory == e.general_category && (
+            <div className="my-6" key={e.id}>
+              <h6 className="font-semibold">{e.plural}</h6>
+              {categories.map((category) => (
+                category.main_category == e.id && category.gender.includes(gender) && (
+                  <button
+                    className="my-4 block"
+                    onClick={() => handleClickCategory(category.plural)}
+                    key={category.id}
+                  >
+                    {category.plural}
+                  </button>
+                )
+              ))}
             </div>
-            )}
-        </ul>
-    )
-}
+          )}
+        </div>
+      ))}
+    </ul>
+  );
+};

d6e5ce29a4c7813e931140df6a4e253ac412bab4
Vasco Saraiva <vasco.saraiva@ua.pt>
2024-01-05 13:13:34 +0000

    Modifiquei componente CardProduct.jsx. Criei componente complementar

--- components/CardProduct.jsx
+++ components/CardProduct.jsx
@@ -6,22 +6,23 @@ import PriceIntervalCardProduct from './PriceIntervalCardProduct';
 
 const CardProduct = ({ product }) => {
 
-    
+
     const categoryName = categories.find(element => element.id === product.categories.id).singular
 
     return (
         <article className='w-full max-w-[460px] flex-5'>
-           
-            <div className='w-full rounded border-grey border aspect-square relative flex justify-center items-center'>
+
+            <div className='w-full rounded border-grey border aspect-[3/4] relative flex justify-center items-center'>
 
                 <Link href={`/product/mulher/${product.id}`}>
-                <Image
-                    src={product.images[0].url}
-                    alt={product.images[0].alt}
-                    fill={true}
-                />
+                    <Image
+                        src={product.images[0].url}
+                        alt={product.images[0].alt}
+                        className='object-cover scale-90'
+                        fill={true}
+                    />
                 </Link>
-                
+
                 <div className='absolute top-2.5 px-4 w-full flex items-center justify-between'>
                     <Link href={`/brands/mulher/${product.brands.name}`}>
                         <Image

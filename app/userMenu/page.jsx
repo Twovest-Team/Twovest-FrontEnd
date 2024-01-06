26ecf63c306190c38801eaf1d9e337772e5e4178
Daniela Silva <danif.gsilva2000@gmail.com>
2024-01-06 18:32:21 +0000

    userMenu with button to open and close modal

---
+++ app/userMenu/page.jsx
@@ -0,0 +1,51 @@
+"use client"
+
+import AutoModeOutlinedIcon from '@mui/icons-material/AutoModeOutlined';
+import ArrowCircleUpOutlinedIcon from '@mui/icons-material/ArrowCircleUpOutlined';
+import ArrowForwardOutlinedIcon from '@mui/icons-material/ArrowForwardOutlined';
+import { useState } from 'react';
+
+const userMenu = () => {
+ const [modalOpen, setModalOpen] = useState(false);
+
+ const toggleModal = () => {
+    setModalOpen(!modalOpen);
+ };
+ return (
+    <div>
+      <button onClick={toggleModal}>Open Modal</button>
+      {modalOpen && (
+        <div className='user_modal'>
+          <div className="modal_header">
+            <div className='body_semibold'>Margarida Ferreira</div>
+                    <button className='button_id'>ID: 12a30sv</button>
+                    <div className='div_grey-line'/>
+          </div>
+            <div className='modal_body'>
+                <button className='button_default'>Perfil</button>
+                <button className='button_points'> <AutoModeOutlinedIcon fontSize='15px' /> Pontos & Cupões</button>
+                <div className='div_grey-line'/>
+            </div>
+     
+                <div className='modal_body'>
+                    <button className='button_submit-look'> <ArrowCircleUpOutlinedIcon fontSize='15px' />Submeter novo look</button>
+                    <button className='button_default'>Gerir os meus looks</button>
+                    <button className='button_default'>Ver coleções de looks</button>
+                    <div className='div_grey-line'/>
+                </div>
+      
+                <div className='modal_footer'>
+                    <button className='button_default'>Histórico de compras</button>
+                    <button className='button_default'>Definições da conta</button>
+                    <button className='button_logout'>
+                        Sair → 
+                    </button>
+                </div>
+            <button onClick={toggleModal}>Close Modal</button>
+        </div>
+      )}
+    </div>
+ );
+};
+
+export default userMenu;

ab001e9ce09ab7b155e9d0c898c60d710b95b5a5
Jónatas Roque <jonatasroque@ua.pt>
2024-01-06 17:04:11 +0000

    fix titulo de menus

--- components/navbar/CategoriesMenu.jsx
+++ components/navbar/CategoriesMenu.jsx
@@ -11,11 +11,11 @@ export const CategoriesMenu = ({idCategory, categoryOpen, toggleCategory, gender
             <div className={`${categoryOpen ? "translate-x-0" : "-translate-x-full"}
             bg-white z-50 overflow-scroll h-full w-screen fixed top-0 left-0 transition-transform duration-400 ease-in-out`}>
         
-                <div className="flex  items-center border-b-grey border-b-2">
-                    <div className="flex mx-4">
-                        <div onClick={()=> toggleCategory(idCategory)}><ArrowBackIosIcon/></div>
+                <div className="flex  items-center border-b-grey border-b-2  h-[66px]">
+                    <div className="flex ml-4 mr-2">
+                        <div onClick={()=> toggleCategory(idCategory)}><ArrowBackIosIcon className="text-[18px]"/></div>
                     </div>
-                    <div className="flex my-5">
+                    <div className="flex">
                         <h6 className="font-semibold">{general_categories.find(e=>e.id==idCategory).name}</h6>
                     </div> 
                 </div>

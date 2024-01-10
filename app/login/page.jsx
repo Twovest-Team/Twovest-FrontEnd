25d5a5a0cbba1b8a172c027f84efb64610e7f372
Jónatas Roque <jonatasroque@ua.pt>
2024-01-10 15:46:46 +0000

    design login page

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -5,6 +5,10 @@ import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { useAppDispatch } from "@/redux/hooks";
 import { changeUserData } from "@/redux/slices/userSlice";
+import NavigationTitle from "@/components/providers/NavigationTitle";
+import { Buttons } from "@/components/buttons/Buttons";
+import Link from "next/link";
+
 
 export default function LoginPage() {
     const router = useRouter()
@@ -13,6 +17,7 @@ export default function LoginPage() {
 
     const dispatch = useAppDispatch()
     const supabase = createClientComponentClient();
+
 
     useEffect(() => {
         async function getUser() {
@@ -34,6 +39,7 @@ export default function LoginPage() {
         })
     }
 
+    
     const handleLogout = async () => {
         await supabase.auth.signOut();
         router.refresh();
@@ -43,12 +49,13 @@ export default function LoginPage() {
 
 
     if (loading) {
-        return <h1>loading..</h1>
+        return <h1 className="h-screen text-center"><div className="mt-4">Loading...</div></h1>
     }
 
     if (user) {
         return (
-            <div className="h-screen flex flex-col justify-center items-center bg-gray-100">
+            <div className="h-screen bg-gray-100">
+                <NavigationTitle/>
                 <div className="bg-white dark:bg-gray-900 p-8 rounded-lg shadow-md w-96 text-center">
                     <h1 className="mb-4 text-xl font-bold text-gray-700 dark:text-gray-300">
                         You&apos;re already logged in
@@ -65,17 +72,48 @@ export default function LoginPage() {
     }
 
     return (
-        <main className="h-screen flex items-center justify-center bg-gray-800 p-6">
-            <div className="bg-gray-900 p-8 rounded-lg shadow-md w-96">
+        <>
+        <NavigationTitle titleText={"Iniciar sessão"}/>
+         <main className="h-screen  p-6">
+
+         <input type="text" placeholder="Email" className="px-4 py-4 w-full mb-4 rounded border border-grey"/>
+         <input type="password" placeholder="Password" className="px-4 py-4 w-full rounded border border-grey"/>
+        <Buttons btnState={"defaultMain"} text={"Iniciar sessão"} btnSize={"menuSize"}/>
+
+            <div className="flex my-12 items-center">
+                <div className="border-b border-grey w-full"></div>
+                <div className="font-semibold mx-4">Ou</div>
+                <div className="border-b border-grey w-full"></div>
+            </div>
+            
+            <div className="">
     
-                <button
+                <Buttons
                     onClick={handleSignInGoogle}
-                    className="w-full p-3 rounded-md bg-green-600 text-white hover:bg-green-700 focus:outline-none"
+                    btnState={"secondaryMain"} text={"Continuar com Google"} btnSize={"menuSize"}
                 >
                     Sign In Google
-                </button>
+                </Buttons>
+                <Buttons
+                    
+                    btnState={"secondaryMain"} text={"Continuar com Facebook"} btnSize={"menuSize"}
+                >
+                    Sign In Google
+                </Buttons>
+                <Buttons
+                    
+                    btnState={"secondaryMain"} text={"Continuar com Apple"} btnSize={"menuSize"}
+                >
+                    Sign In Google
+                </Buttons>
             </div>
+
+            <div className="text-center mt-20">Não tens conta? <Link href={"/register"} className="text-primary_main font-semibold" >Regista-te com email.</Link>
+            </div>
+
         </main>
+        </>
+       
     )
 
 }

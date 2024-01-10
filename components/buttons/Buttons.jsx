167c835b311394d0d7ae601391e5d7dd4f709d66
gcalcada <gcalcada7@ua.pt>
2024-01-10 19:40:39 +0000

    Inclusao de um carousel e ligeiras alteraçoes nos botoes e verificaçao dos modais adicionei media querys no globals.css

--- components/buttons/Buttons.jsx
+++ components/buttons/Buttons.jsx
@@ -41,18 +41,18 @@ modalSize: 'w-full h-14 gap-12 items-center',
         <Buttons btnState="none"  btnSize="justIcons" icon="filter"/>
         </div>
         <div className="mx-6">
-        <Buttons btnState="defaultMain" text="Pagar agora" btnSize="mediumSize"/> 
+        <Buttons btnState="defaultMain" text="Pagar agora" btnSize="mediumSizeSocials"/> 
   
     <Buttons btnState="defaultMain" text="Sustentável" icon="navigateNext" btnSize="menuSize" />
     <Buttons btnState="defaultMain" text="Promoções" icon="offer" btnSize="menuSize" />
 
       <Buttons aria-label="Next" btnState="defaultMain" text="Proximo" icon="navigateNext" btnSize="mediumSize"/>
-      <Buttons btnState="secondaryMain" text="Registar Conta" btnSize="mediumSize"/>
+      <Buttons btnState="defaultMain" text="Registar Conta" btnSize="mediumSizeSocials"/>
       <Buttons btnState="secondaryMain" text="Continuar com Google" btnSize="mediumSizeSocials" icon="google"/>
       <Buttons btnState="secondaryMain" text="Continuar com Facebook" btnSize="mediumSizeSocials" icon="facebook"/>
       <Buttons btnState="secondaryMain" text="Continuar com Maça" btnSize="mediumSizeSocials" icon="apple"/>
       <Buttons btnState="secondaryMain" text="Redefinir" btnSize="mediumSizeSocials" icon="redifine"/>
-      <Buttons btnState="defaultMain" text="Voltar a loja" btnSize="mediumSizeSocials"/>
+      <Buttons btnState="defaultMain" text="Registar Conta" btnSize="mediumSizeSocials"/>
       </div>
 
 */
@@ -68,7 +68,7 @@ import ReplayIcon from '@mui/icons-material/Replay';
 import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import FilterListOutlinedIcon from '@mui/icons-material/FilterListOutlined';
-export const Buttons = ({ btnState, text, icon, btnSize, onClick }) => {
+export const Buttons = ({ btnState, text, icon, btnSize }) => {
   
 
 
@@ -84,13 +84,13 @@ export const Buttons = ({ btnState, text, icon, btnSize, onClick }) => {
   };
   const sizes = {
     modalSize: 'w-full h-14 gap-12 items-center',
-    smallSize: 'w-[65%] h-14 gap-12 items-center text-center justify-between items-center',
+    smallSize: 'w-full h-14 sm:h-14 md:h-14 lg:h-14 xl:h-14 flex items-center justify-between gap-2 sm:gap-4 text-center',
     menuSize: 'w-full h-14 py-2 px-4 mt-4 flex gap-12 text-center justify-between items-center', //usado apenas no scroll menu
     mediumSize: 'w-full h-full px-8 flex py-3.5 mt-4 gap-12 items-center justify-between', // usado no geral em açoes como concluir, enviar, carregar mais, proximo passo etc
     mediumSizeSocials: 'w-full h-55 px-8 py-3.5 mt-4 flex justify-center items-center gap-3 text-center',  // Social netwowrks
     redefineSize: 'w-full h-55 px-3 py-9 mt-4 flex justify-center items-center gap-3 text-center',  //usado para voltar para a galeria e redefinir
     large: 'px-5 py-5  mt-4 text-lg', // caso pretendam um botao maior podem ajustar 
-    justIcons:'w-[18px] ml-auto translate-x-1 justify-center items-center fontSize: 28'    
+    justIcons:'w-10 h-10 mx-auto my-auto justify-center items-center  text-center mt-4'    //quando se quer usar so um icon como botao 
   };
 // iconmap define quais sao os icones nome: [icon material ui ou outros]
   const iconMap = {
@@ -101,6 +101,7 @@ export const Buttons = ({ btnState, text, icon, btnSize, onClick }) => {
     facebook: <FacebookIcon className='text-white ' />, 
     apple: <AppleIcon className='text-white ' />, 
     redifine: <ReplayIcon className='text-white transform rotate-90 ' />, 
+    redifine2: <ReplayIcon className='text-grey transform rotate-90 ' />, 
     offer: <LocalOfferIcon className='text-white ' />, 
     delete:  <DeleteOutlineOutlinedIcon className= " text-secondary" />,
     favorite: <FavoriteBorderIcon className='text-secondary'/>,
@@ -110,9 +111,11 @@ export const Buttons = ({ btnState, text, icon, btnSize, onClick }) => {
 const selectedIcon = iconMap[icon] || null;
 
   return (
-    <button className={`flex font-semibold font-inter aria-label="${text}" rounded ${sizes[btnSize]} ${StyledState[btnState]}`}>
+    <button className={`flex font-semibold font-inter aria-label="${text}" rounded ${sizes[btnSize]} ${StyledState[btnState]} mx-auto items-center`}>
+    
        {['google', 'facebook', 'apple', 'redifine'].includes(icon) && <span>{selectedIcon}</span>}
-       <span className={`text-white min-w-fit ${!selectedIcon ? 'mx-auto pl-12' : ''}`}>{text}</span>
+       <span className={`text-white  ${!selectedIcon ? 'text-center' : ''}`}>{text}</span>
+    
 {!['google', 'facebook', 'apple', 'redifine'].includes(icon) && <span>{selectedIcon}</span>}
     </button>
   );

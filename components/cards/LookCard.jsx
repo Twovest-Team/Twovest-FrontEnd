a0abbfe0e5922a122a0cc8a7b7efe1356ad27b04
jkapa17 <tomaspaixao2002@gmail.com>
2024-01-11 22:48:12 +0000

    Card da galeria com itens da base de dados As cards mostradas na página da galeria respondem agora às informações provenientes da base de dados, através dos endpoints previamente criados. Tanto a imagem, como o nome e avatar de utilizador e o número de upvotes, são provenientes da base de dados. Adicionalmente, foi criado um esqueleto da card enquanto esta carrega

--- components/cards/LookCard.jsx
+++ components/cards/LookCard.jsx
@@ -4,9 +4,6 @@ import LookUpvoteButton from "../buttons/icons/LookUpvoteButton";
 //Botão de guardar (bookmark) no canto inferior direito da card de look
 import SaveButton from "../buttons/icons/SaveButton";
 
-// Imagem de placeholder do componente
-import ImagemLook from "@/public/images/gallery/looks/looks_mulher1.png";
-
 // Componente que mostra o nome de utilizador apenas se estiver em vista de 1 coluna
 import LookUsername from "../items/LookUsername";
 
@@ -15,28 +12,30 @@ Utiliza tanto o componente de Upvote (LookUpvoteButton) como de guardar look, ic
 (SaveButton)
  */
 
-export default function LookCard(look) {
+export default function LookCard({ look, gender }) {
   return (
     <div className="w-full max-w-[460px]">
-      <div className='w-full rounded aspect-[17/26] relative flex justify-center items-center'>
+      <div className="w-full rounded aspect-[17/26] relative flex justify-center items-center">
         <Image
-          src={ImagemLook}
+          src={look.url_image}
           alt="Look da galeria"
-          className='object-cover scale-100'
+          className="object-cover scale-100"
           fill={true}
         />
-        <LookUpvoteButton />
+        <LookUpvoteButton upvotes={look.upvotes} />
       </div>
       <div className="flex flex-wrap justify-between items-center">
         <div className="flex flex-wrap items-center mt-3.5">
           <div className="rounded-full w-10 h-10 overflow-hidden mr-2">
             <Image
-              src={ImagemLook}
+              src={look.users.img}
               alt="Look da galeria"
+              width={100}
+              height={100}
               style={{ objectFit: "contain" }}
             />
           </div>
-          <LookUsername username="Deolinda.Soares51"/>
+          <LookUsername username={look.users.name} />
         </div>
         <div className="mt-3.5">
           <SaveButton />

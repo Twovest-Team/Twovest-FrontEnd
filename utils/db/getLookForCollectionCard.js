471a848a68202c4f7c012ef0066377652a4accce
jkapa17 <tomaspaixao2002@gmail.com>
2024-01-12 15:44:21 +0000

    Endpoint com informações necessárias para o perfil Endpoint (getInfoForProfilePage) que recebe todas as informações que vão ser visualizadas no perfil de um user, bastando apenas receber o seu ID. Isto inclui informações do utilizador (nome, email,etc...), as coleções que tem guardadas e também os looks que publicou

---
+++ utils/db/getLookForCollectionCard.js
@@ -0,0 +1,29 @@
+import { supabase } from "@/utils/db/supabase";
+
+const getLookForCollectionCard = async (id_collection) => {
+  const { data, error } = await supabase
+    .from("collections_has_looks")
+    .select(
+      `
+    id_look,
+    created_at,
+    looks(
+        id,
+        url_image
+    )
+`
+    )
+    .eq("id_collection", id_collection)
+    .order('created_at', { ascending: false })
+    .limit(3)
+
+   
+
+  if (error) {
+    console.log(error);
+  } else {
+    return data;
+  }
+};
+
+export default getLookForCollectionCard;

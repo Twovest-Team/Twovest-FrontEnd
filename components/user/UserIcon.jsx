6816f69c10b2bdf9e05d9b2269e75c3875e67d62
Jónatas Roque <jonatasroque@ua.pt>
2024-01-17 06:10:10 +0000

    UserIcon componente feito e com parametros de entrada

--- components/user/UserIcon.jsx
+++ components/user/UserIcon.jsx
@@ -1,7 +1,42 @@
-import Image from "next/image"
+import Image from "next/image";
+import Link from "next/link";
 
-export const UserIcon = ({url, userState}) =>{
-    return(
-        <Image src={url} className="rounded-full border-grey border" width={24} height={24} alt="profile image"/>
-    )
+export const UserIcon = ({url, userRole, size, userName, userId}) =>{
+
+    if (size === "small") {
+        imageSize = 25;
+      } else if (size === "medium") {
+        imageSize = 35;
+      } else if (size === "large") {
+        imageSize = 45;
+      } else {
+        imageSize = 25;
+      }
+
+    if (userRole === "influencer") {
+        return (
+          <Link href={`/profile/id=${userId}`}>
+            <Image
+              src={url}
+              className="rounded-full border-primary_main border"
+              width={imageSize}
+              height={imageSize}
+              alt={`profile image ${userName}`}
+            />
+          </Link>
+        );
+      } else {
+        return (
+          <div>
+            {/* Render some other content if userRole is not "influencer" */}
+            <Image
+              src={url}
+              className="rounded-full border-grey border"
+              width={imageSize}
+              height={imageSize}
+              alt={`profile image ${userName}`}
+            />
+          </div>
+        );
+      }
 }

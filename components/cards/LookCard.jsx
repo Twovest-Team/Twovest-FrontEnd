3f8bdacbcae5118755f2a0c2a52063002f6f44c1
gcalcada <gcalcada7@ua.pt>
2024-01-17 01:43:10 +0000

    s

--- components/cards/LookCard.jsx
+++ components/cards/LookCard.jsx
@@ -14,33 +14,48 @@ Utiliza tanto o componente de Upvote (LookUpvoteButton) como de guardar look, ic
  */
 
 export default function LookCard({ look, slider, nome, avatar }) {
-  console.log(look.users.id)
   return (
-    <div className={`w-full ${!slider ? 'max-w-[460px]' : slider === true && 'w-[160px] min-w-[160px]'} `}>
-      <Link href={`/gallery/look/${look.id}`} className="w-full aspect-[17/26] relative flex justify-center items-center">
+    <div
+      className={`w-full ${
+        !slider ? "max-w-[460px]" : slider === true && "w-[160px] min-w-[160px]"
+      } `}
+    >
+      <Link
+        href={`/gallery/look/${look.id}`}
+        className="w-full aspect-[17/26] relative flex justify-center items-center"
+      >
         <Image
           src={!slider ? look.url_image : slider === true && look.url_image}
           alt="Look da galeria"
           className="object-cover scale-100 rounded"
           fill={true}
+          quality={5}
         />
-        {!slider ? <LookUpvoteButton upvotes={look.upvotes} /> : slider === true && null}
+        {!slider ? (
+          <LookUpvoteButton upvotes={look.upvotes} />
+        ) : (
+          slider === true && null
+        )}
       </Link>
       <div className="flex flex-wrap justify-between items-center">
-
-        <Link href={`/profile?id=${look.users.id}`} className="flex gap-2  min-w-0 items-center mt-3.5">
-
+        <Link
+          href={`/profile?id=${look.users.id}`}
+          className="flex gap-2  min-w-0 items-center mt-3.5"
+        >
           <Image
             src={!slider ? look.users.img : slider === true && avatar}
             alt="Look da galeria"
             width={35}
             height={35}
+            quality={5}
             className="rounded-full"
           />
 
-          <LookUsername slider={true} username={!slider ? look.users.name : slider === true && nome} />
+          <LookUsername
+            slider={true}
+            username={!slider ? look.users.name : slider === true && nome}
+          />
         </Link>
-
 
         <div className="mt-3.5">
           {!slider ? <SaveButton /> : slider === true && null}

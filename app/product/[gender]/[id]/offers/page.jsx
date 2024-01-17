7a1501e0f3f689bc2dd10cc1808c6f7d782c838f
Daniela Silva <danif.gsilva2000@gmail.com>
2024-01-17 23:47:01 +0000

    New commit with changes

--- app/product/[gender]/[id]/offers/page.jsx
+++ app/product/[gender]/[id]/offers/page.jsx
@@ -3,10 +3,30 @@ import FiltersProduct from "@/components/filters_product/filtersProduct";
 import ProductsFiltered from "@/components/filters_product/productsFiltered";
 import getProductOffers from "@/utils/db/getProductOffers";
 import getProductById from "@/utils/db/getProductById";
+import { categories, main_categories } from "@/constants";
 
-async function AllOffers({ params }) {
+async function AllOffers({ params, searchParams }) {
   const data = await getProductOffers(params.id);
   const productData = await getProductById(params.id, params.gender);
+  const productType = main_categories.find((category) => category.id.toString() == productData.categories.main_category);
+  const productCategory = categories.find((category) => category.id.toString() == productData.categories.id);
+  const color = searchParams.color;
+  const condition = searchParams.condition;
+  const size = searchParams.size;
+
+  let filteredData = data;
+
+  if (color) {
+    filteredData = data.filter((offer) => offer.colors.name === color);
+  }
+
+  if (condition) {
+    filteredData = data.filter((offer) => offer.conditions.name === condition);
+  }
+
+  if (size) {
+    filteredData = data.filter((offer) => offer.sizes.size === size);
+  }
 
   return (
     <>
@@ -14,11 +34,11 @@ async function AllOffers({ params }) {
         <ArrowBackIosIcon />
         <p className="text-black text-[23.04px] font-semibold">Ofertas</p>
         <p className="text-neutral-400 text-base font-normal leading-snug">
-          {data.length} opções
+          {filteredData.length} opções
         </p>
       </div>
-      <FiltersProduct />
-      <ProductsFiltered offers={data} discount={productData.discount} />
+      <FiltersProduct productType={productType.singular} productId={params.id} gender={params.gender}/>
+      <ProductsFiltered offers={filteredData} discount={productData.discount} product={productData} productCategory={productCategory} />
     </>
   );
 }

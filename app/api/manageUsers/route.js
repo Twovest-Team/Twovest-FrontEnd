5208102e9c831a7e06fa647b775246c1d76e2629
Jónatas Roque <jonatasroque@ua.pt>
2024-01-21 21:02:28 +0000

    fix auth e alt img login providers

--- app/api/manageUsers/route.js
+++ app/api/manageUsers/route.js
@@ -3,21 +3,17 @@ import handleUsers from '@/utils/db/handleUsers';
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 
+
 export async function GET(request) {
-    try {
-        const cookieStore = cookies();
-        const supabase = createServerComponentClient({ cookies: () => cookieStore });
-        const { data: { user } } = await supabase.auth.getUser();
 
-        if (user) {
-            handleUsers(user.user_metadata);
-        }
+    const cookieStore = cookies();
+    const supabase = createServerComponentClient({ cookies: () => cookieStore });
+    const { data: { user } } = await supabase.auth.getUser()
 
-        return NextResponse.redirect(process.env.NEXT_PUBLIC_URL);
-    } catch (error) {
-        console.error('Error in GET request:', error);
-
-        // You may want to handle the error appropriately, such as redirecting to an error page.
-        return NextResponse.error(new Error('Unauthorized access'));
+    if (user) {
+        handleUsers(user.user_metadata)
     }
+
+
+    return NextResponse.redirect(process.env.NEXT_PUBLIC_URL)
 }

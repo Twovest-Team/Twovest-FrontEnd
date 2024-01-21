ef67233116d8d43716bbf9c557cdeeeba5ba6c78
Jónatas Roque <jonatasroque@ua.pt>
2024-01-21 21:08:12 +0000

    fix route

--- app/api/manageUsers/route.js
+++ app/api/manageUsers/route.js
@@ -3,17 +3,21 @@ import handleUsers from '@/utils/db/handleUsers';
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 
-
 export async function GET(request) {
+    try {
+        const cookieStore = cookies();
+        const supabase = createServerComponentClient({ cookies: () => cookieStore });
+        const { data: { user } } = await supabase.auth.getUser();
 
-    const cookieStore = cookies();
-    const supabase = createServerComponentClient({ cookies: () => cookieStore });
-    const { data: { user } } = await supabase.auth.getUser()
+        if (user) {
+            handleUsers(user.user_metadata);
+        }
 
-    if (user) {
-        handleUsers(user.user_metadata)
+        return NextResponse.redirect(process.env.NEXT_PUBLIC_URL);
+    } catch (error) {
+        console.error('Error in GET request:', error);
+
+        // You may want to handle the error appropriately, such as redirecting to an error page.
+        return NextResponse.error(new Error('Unauthorized access'));
     }
-
-
-    return NextResponse.redirect(process.env.NEXT_PUBLIC_URL)
 }

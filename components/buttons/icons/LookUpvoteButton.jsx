fe642d345346019e5a0358764573c0c09a51c9e6
jkapa17 <tomaspaixao2002@gmail.com>
2024-02-24 19:12:01 +0000

    Arredondar cantos da card da galeria O canto superior-direito do botão de upvote da card da galeria foi arredondado. Previamente não estava devido a um pequeno erro na classe de tailwind

--- components/buttons/icons/LookUpvoteButton.jsx
+++ components/buttons/icons/LookUpvoteButton.jsx
@@ -6,7 +6,7 @@ import formatCompactNumbers from "@/utils/formatCompactNumbers";
 export default function LookUpvoteButton({ upvotes }) {
   return (
     <div
-      className="absolute top-0 right-0 w-[70px] h-[45px] bg-black rounded-tr-[5px]} rounded-bl-[5px] justify-center items-center inline-flex"
+      className="absolute top-0 right-0 w-[70px] h-[45px] bg-black rounded-tr-[5px] rounded-bl-[5px] justify-center items-center inline-flex"
     >
       <ForwardOutlinedIcon className="text-white -rotate-90" />
       <div className="text-center">

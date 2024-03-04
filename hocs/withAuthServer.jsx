f5499e037cf06f48b934860686b74980cd7183a9
gcalcada <gcalcada7@ua.pt>
2024-03-04 21:38:32 +0000

    conflits

--- hocs/withAuthServer.jsx
+++ hocs/withAuthServer.jsx
@@ -1,17 +1,13 @@
-import getUserByEmailServer from '@/utils/db/getUserByEmailServer'
+import getUserByEmailServer from "@/utils/db/auth/getUserByEmailServer";
 
 const withAuthServer = (WrappedComponent) => {
+  const WithAuth = async (props) => {
+    let authData = await getUserByEmailServer();
 
-    const WithAuth = async(props) => {
-        
-        let authData = await getUserByEmailServer() 
+    return <WrappedComponent {...props} currentUser={authData || null} />;
+  };
 
-        return <WrappedComponent {...props} currentUser={authData || null} />;
+  return WithAuth;
+};
 
-    }
-
-    return WithAuth
-
-}
-
-export default withAuthServer
+export default withAuthServer;

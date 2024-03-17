cb4b275ec4091edeb6b6374c062e025334a7f105
Vasco Saraiva <vasco.saraiva@ua.pt>
2024-03-17 18:46:38 +0000

    more bug corrections

--- hooks/useGender.jsx
+++ hooks/useGender.jsx
@@ -1,11 +1,9 @@
 import { useAppSelector, useAppDispatch } from "@/redux/hooks";
 import { updateGender } from "@/redux/slices/genderSlice";
-import { useRouter } from 'next/navigation'
 
 const useGender = () => {
    const dispatch = useAppDispatch();
-   const gender = useAppSelector(state => state.gender.data);
-   const router = useRouter()
+   const gender = useAppSelector(state => state.gender.data) || 'women'
    
    const setGender = (newGender) => {
         dispatch(updateGender(newGender)); 

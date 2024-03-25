f4c72417dbd0e07ed49780b571d8cdae95eb8c04
Jónatas Roque <jonatasroque@ua.pt>
2024-03-25 15:49:38 +0000

    ajustes na paginação e navegação, com uso de imagens vindos do bucket onboarding

--- app/landing/page.jsx
+++ app/landing/page.jsx
@@ -11,6 +11,8 @@ import getGender from "@/utils/getGender";
 import Button from "@/components/buttons/Button";
 import { useState } from "react";
 import { onboardingData } from "@/constants";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 // Se utilizador já tem um género selecionado deve ser redirecionado para a homepage
 export default function Landing() {
@@ -37,35 +39,46 @@ export default function Landing() {
     setCurrentPage((prevPage) => (prevPage > 1 ? prevPage - 1 : prevPage));
   };
 
+  const handleSkip = () => {
+    setCurrentPage(10);
+  };
+
   const currentPageData = onboardingData.find((item) => item.page === currentPage);
+  
+  const backgroundImageStyle = currentPageData.img !== "" ? {backgroundImage: `url(${currentPageData.img})`} : {backgroundColor: '#05CE86', backgroundRepeat: 'no-repeat', backgroundPosition: 'center 30%', backgroundImage: `url("https://nchduotxkzvmghizornd.supabase.co/storage/v1/object/public/onboarding/twovest_icon.svg")`};
 
   return (
+
     <div className="relative h-[100vh] mt-[-75px] ">
-    <div className="rounded-bl-[12%] bg-primary_main h-full w-full"></div>
+    <div
+      id="onboardingImg"
+      className={`rounded-bl-[12%] h-full w-full ${currentPageData.img !== "" ? `bg-cover bg-center` : `bg-primary_main`}`}
+      style={backgroundImageStyle}
+    ></div>
     <div className="absolute bottom-0 w-full bg-white rounded-tr-[12%] h-[40%]">
       <div className="mx-6 break-words">
-        <h5 className="mt-10 mb-4 font-semibold">{currentPageData.titulo}</h5>
+        <h5 className="mt-9 mb-3.5 font-semibold">{currentPageData.titulo}</h5>
         <p className="text-secondary text-justify">{currentPageData.texto}</p>
       </div>
-      <div className="justify-between bottom-10 absolute flex w-full px-6 items-center">
+      <div className="justify-between bottom-6 absolute flex w-full px-6 items-center">
         {currentPage > 1 && (
           <>
-            <div className="cursor-pointer underline text-primary_main" onClick={previousPage}>
-              Página Anterior
+            <div className="cursor-pointer rounded-full border border-primary_main w-12 h-12 flex items-center justify-center text-primary_main" onClick={previousPage}>
+              <ArrowBackIcon />
             </div>
-            <div className="cursor-pointer underline text-primary_main" onClick={nextPage}>
-              Próxima Página
+            <div className="cursor-pointer rounded-full border-primary_main bg-primary_main w-12 h-12 flex items-center justify-center text-white" onClick={nextPage}>
+              <ArrowForwardIcon/>
             </div>
           </>
         )}
         {currentPage === 1 && (
           <>
-            <div className="underline text-primary_main">Saltar</div>
+            <div className="cursor-pointer underline text-primary_main" onClick={handleSkip}>Saltar</div>
             <Button children={"Começar ->"} onClick={nextPage} />
           </>
         )}
         {currentPage === totalPages && (
-          <div className="cursor-pointer underline text-primary_main">Saltar</div>
+          <div className="cursor-pointer underline text-primary_main" onClick={handleSkip}>Saltar</div>
         )}
       </div>
     </div>

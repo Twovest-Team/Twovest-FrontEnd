2d105b0e462a4243176b932c3be0abbdbbe06f57
jkapa17 <tomaspaixao2002@gmail.com>
2024-03-27 14:56:50 +0000

    Resolução de acessibilidade de links semânticos Otimização de acessibilidade do problema links semânticos (WCAG 2.0 A 4.1.1) em diversas páginas e componentes

--- components/collections/CollectionList.jsx
+++ components/collections/CollectionList.jsx
@@ -1,55 +1,56 @@
-import CollectionPreview from './CollectionPreview'
+import CollectionPreview from "./CollectionPreview";
 import SearchIcon from "@mui/icons-material/Search";
-import { Fragment } from 'react';
+import { Fragment } from "react";
 
-const CollectionList = ({ collections, ownerId, search, isOwner, toSaveLook, ownerFirstName }) => {
-    return (
+const CollectionList = ({
+  collections,
+  ownerId,
+  search,
+  isOwner,
+  toSaveLook,
+  ownerFirstName,
+}) => {
+  return (
+    <Fragment key={"CollectionListFragment"}>
+    
+      {/* IF THERE ARE ANY COLLECTIONS */}
+      {collections && (
+        <ul className="flex flex-col items-start self-stretch gap-4 ">
+          {search && (
+            <button className="profile_search-collections">
+              <SearchIcon />
+              Procurar coleções
+            </button>
+          )}
+
+          
+          {collections.map((collection) => (
+            <li key={collection.id_collection} className="w-full">
+            <CollectionPreview
+              toSaveLook={toSaveLook}
+              userId={ownerId}
+              collection={collection}
+              key={collection.id_collection}
+            />
+            </li>
+          ))}
+          </ul>
+      )}
+
+      {/* IF THERE IS NO COLLECTION */}
+      {!collections && (
         <div className="flex flex-col items-start self-stretch gap-4 ">
+        <div className="text-secondary">
+          {isOwner && <p>Ainda não criaste nenhuma coleção.</p>}
 
-            {/* IF THERE ARE ANY COLLECTIONS */}
-            {collections &&
-                <Fragment key={"CollectionListFragment"}>
-
-                    {search &&
-                        <button className="profile_search-collections">
-                            <SearchIcon />
-                            Procurar coleções
-                        </button>
-                    }
-
-
-                    {collections.map(collection => (
-                        <CollectionPreview
-                            toSaveLook={toSaveLook}
-                            userId={ownerId}
-                            collection={collection}
-                            key={collection.id_collection}
-                        />
-                    ))}
-                </Fragment>
-            }
-
-
-            {/* IF THERE IS NO COLLECTION */}
-            {!collections &&
-                <div className="text-secondary">
-
-                    {isOwner &&
-                        <p>Ainda não criaste nenhuma coleção.</p>
-                    }
-
-                    {!isOwner && ownerFirstName &&
-                        <p>{ownerFirstName} não tem coleções disponíveis.</p>
-                    }
-
-                </div>
-            }
-
+          {!isOwner && ownerFirstName && (
+            <p>{ownerFirstName} não tem coleções disponíveis.</p>
+          )}
         </div>
-    )
-}
+        </div>
+      )}
+    </Fragment>
+  );
+};
 
-
-
-
-export default CollectionList
+export default CollectionList;

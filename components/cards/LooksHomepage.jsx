6b3b8ac052a47a42248933bc423eb0547d61bde2
jkapa17 <tomaspaixao2002@gmail.com>
2024-03-27 15:11:28 +0000

    Fix de bug q não mostrava looks na página inicial

--- components/cards/LooksHomepage.jsx
+++ components/cards/LooksHomepage.jsx
@@ -1,23 +1,25 @@
 import ContentSlider from "../sliders/ContentSlider";
 import LookCard from "./LookCard";
 
-export const LooksHomepage = ({data}) => {
-    
-    const looks = data;
-    //console.log(looks)
+export const LooksHomepage = ({ data }) => {
+  const looks = data;
+  //console.log(looks)
 
-      return(
-        
-       <ContentSlider>
-            {looks.map((item) => {
-                <li key={item.id}>
-                return(<LookCard key={item.id} look={item} name={item.users.name} avatar={item.users.img} slider={true}/>)  
-                </li> 
-            }
-            )}  
-        </ContentSlider>
-        
-    )  
-    
-    
-}
+  return (
+    <ContentSlider>
+      {looks.map((item) => {
+        return (
+          <li key={item.id}>
+            <LookCard
+              key={item.id}
+              look={item}
+              name={item.users.name}
+              avatar={item.users.img}
+              slider={true}
+            />
+          </li>
+        );
+      })}
+    </ContentSlider>
+  );
+};

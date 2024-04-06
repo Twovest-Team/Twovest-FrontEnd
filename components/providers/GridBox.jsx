cfd526a28266bf6ed6d94e73cfbe30067542ed55
Vasco Saraiva <vasco.saraivaua.pt>
2024-04-06 20:04:24 +0100

    Update slider. Update GridBox

--- components/providers/GridBox.jsx
+++ components/providers/GridBox.jsx
@@ -7,7 +7,7 @@ import { useAppSelector } from '@/redux/hooks';
 const GridBox = ({ children, loader, fixed }) => {
 
 
-    let cols;
+    const [cols, setCols] = useState('max-[350px]:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4');
     const currentView = useAppSelector(state => state.layoutViews.currentValue)
     const [loading, setLoading] = useState(true)
 
@@ -15,21 +15,27 @@ const GridBox = ({ children, loader, fixed }) => {
         setLoading(false)
     }, [])
 
-
-    if (currentView) {
-        switch (currentView) {
-            case 1: cols = 'grid-cols-1'
-            case 2: cols = 'grid-cols-2'
-            case 3: cols = 'grid-cols-3'
-            case 4: cols = 'grid-cols-4'
-            case 5: cols = 'grid-cols-5'
+    useEffect(() => {
+        if (currentView) {
+            switch (currentView) {
+                case 1: setCols('grid-cols-1')
+                    break;
+                case 2: setCols('grid-cols-2')
+                    break;
+                case 3: setCols('grid-cols-3')
+                    break;
+                case 4: setCols('grid-cols-4')
+                    break;
+                case 5: setCols('grid-cols-5')
+                    break;
+            }
         }
-    }
 
-    if (!currentView) cols = 'max-[350px]:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'
+    }, [currentView])
+
 
     return (
-        <div className={`container w-full grid ${cols} gap-3`}>
+        <div className={`container w-full grid ${cols} gap-3 mb-20`}>
             {!loading ? children : loader}
         </div>
     )

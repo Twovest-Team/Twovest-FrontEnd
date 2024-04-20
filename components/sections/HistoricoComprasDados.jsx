ebba49400738642dbe5ed49b2f89ddf6bdeb861f
Jónatas Roque <jonatasroque@ua.pt>
2024-04-20 22:38:07 +0100

    design da página historico de compras

---
+++ components/sections/HistoricoComprasDados.jsx
@@ -0,0 +1,36 @@
+
+
+export default function HistoricoComprasDados (){
+    return(
+        <div>
+                    <div className="mb-2">
+                        <p className="font-semibold">Data da encomenda:</p>
+                        <p>03/11/2023 - 19:24H</p>
+                    </div>
+                    <div className="my-2">
+                        <p className="font-semibold">Data de entrega:</p>
+                        <p className="text-primary_main">Entregue no dia 9/11/2023</p>
+                    </div>
+                    <div className="my-2">
+                        <p className="font-semibold">Total geral:</p>
+                        <p>95.80€</p>
+                    </div>
+                    <div className="my-2">
+                        <p className="font-semibold">Portes de envio:</p>
+                        <p>1,20€</p>
+                    </div>
+                    <div className="my-2">
+                        <p className="font-semibold">Código de envio:</p>
+                        <p>DHL-BN7000Z1</p>
+                    </div>
+                    <div className="my-2">
+                        <p className="font-semibold">Cupões utilizados:</p>
+                        <p>Não aplicado</p>
+                    </div>
+                    <div className="my-2">
+                        <p className="font-semibold">Pontos ganhos:</p>
+                        <p>76 pontos</p>
+                    </div>
+                </div>
+    )
+}

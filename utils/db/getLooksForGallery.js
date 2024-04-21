d1659114eb14caad93e1515169a26b7d83550b04
Vasco Saraiva <vasco.saraivaua.pt>
2024-04-21 17:33:25 +0100

    Updated gallery looks endpoint

--- utils/db/getLooksForGallery.js
+++ utils/db/getLooksForGallery.js
@@ -1,20 +1,9 @@
 import { supabase } from "@/utils/db/supabase";
-import getLookStyles from "./getLookStyles";
 import getGender from "../getGender";
-import useAuthServer from "@/hooks/server-hooks/useAuthServer";
 
 const getLooksForGallery = async (gender) => {
 
   const genderId = getGender(gender).id
-  const currentUser = await useAuthServer();
-
-  const { data: testeData, error: testeError } = await supabase.rpc
-    ('fetch_gallery_looks',
-      { userid: currentUser.id, genderid: genderId }             
-    )
-  console.log(testeData, testeError)
-
-
 
   const { data, error } = await supabase
     .from('looks')
@@ -28,27 +17,31 @@ const getLooksForGallery = async (gender) => {
         id,
         name,
         img
+    ),
+    looks_has_styles(
+      styles(
+        name
+      )
     )
-`
+    `
     )
     .eq('submission_state', 2)
     .eq('gender', genderId);
 
-  let arrayOfLooks = await Promise.all(
-    data.map(async (element) => {
-      let array = element;
-      const styles = await getLookStyles(element.id);
-      array.styles = styles;
-
-      return array;
-    })
-  );
-
-  if (error) {
-    console.log(error);
-  } else {
-    return arrayOfLooks;
+  function transformLooksObject(looksArray) {
+    return looksArray.map(look => {
+      const styles = look.looks_has_styles.map(item => item.styles.name);
+      const { looks_has_styles, ...rest } = look;
+      return {
+        ...rest,
+        styles
+      };
+    });
   }
+
+  if (data) return transformLooksObject(data);
+  if (error) console.log('error', error);
+
 };
 
 export default getLooksForGallery; 

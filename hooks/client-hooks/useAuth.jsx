0001e78dfd1ea158c71c3fed59a4cdece9df80b7
Vasco Saraiva <vasco.saraivaua.pt>
2024-04-21 17:32:52 +0100

    Updated user endpoint

--- hooks/client-hooks/useAuth.jsx
+++ hooks/client-hooks/useAuth.jsx
@@ -1,32 +1,9 @@
-import { useAppSelector, useAppDispatch } from "@/redux/hooks";
-import { changeUserData } from "@/redux/slices/userSlice";
-import getAuth from "@/utils/db/auth/getAuth";
-import { useEffect, useState } from "react";
+import { useAppSelector } from "@/redux/hooks";
+import { useState } from "react";
 
 const useAuth = () => {
-  const dispatch = useAppDispatch();
   const currentUser = useAppSelector((state) => state.user.data);
-  const [userChecked, setUserChecked] = useState(null);
-
-  useEffect(() => {
-    const getCurrentUser = async () => {
-      try {
-        const userData = await getAuth();
-
-        dispatch(changeUserData(userData));
-
-        if (userData !== null && userData !== undefined) {
-          setUserChecked(true);
-        } else {
-          setUserChecked(false);
-        }
-      } catch (error) {
-        console.error("Erro ao obter dados :", error);
-      }
-    };
-
-    getCurrentUser();
-  }, [dispatch]);
+  const [userChecked, setUserChecked] = useState(true);
 
   return { currentUser, userChecked, setUserChecked };
 };
--- utils/db/auth/getAuth.js
+++ utils/db/auth/getAuth.js
@@ -2,32 +2,44 @@ import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
 export default async function getAuth() {
 
-    let supabase;
+  const supabase = createClientComponentClient();
 
-    supabase = createClientComponentClient();
-    
-    const { data: { user }} = await supabase.auth.getUser();
-    
-      if (user) {
-        const { data, error } = await supabase
-          .from("users")
-          .select(
-            `
-                id,
-                email,
-                name,
-                created_at,
-                img,
-                role
-            `
+  const userData = await supabase.auth.getUser();
+  const email = userData?.data?.user?.user_metadata?.email;
+
+  if (email) {
+    const { data, error } = await supabase
+      .from("users")
+      .select(
+        `
+          id,
+          email,
+          name,
+          created_at,
+          img,
+          role,
+          collections:collections_has_users(
+            id_collection,
+            is_admin,
+            collection_data: collections(
+              created_at,
+              updated_at,
+              name,
+              privacy,
+              share_id,
+              looks: collections_has_looks(
+                id_look
+              )
+            )
           )
-          .eq("email", user.user_metadata.email)
-          .single();
-    
-        if (data) {
-          return data;
-        } else if (error) {
-          console.log(error);
-        }
-      }
+        `
+      )
+      .eq("email", email)
+      .single();
+
+      console.log(data)
+
+    if (data) return data;
+    if (error) console.log(error);
+  }
 }

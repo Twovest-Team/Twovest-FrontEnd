36bae4172e3ebbed1ebab5157f2c6953b543eb99
jkapa17 <jpaixao@ua.pt>
2024-05-15 17:08:04 +0100

    Otimização Endpoint getBrands e getBrandsHomepage A única diferença entre o getBrandsHomepage e o getBrands era o limite de marcas que era pedido na query, como tal, apenas se utiliza agora o Endpoint getBrands, com uma variável que define o limite de pedidos

--- utils/db/getBrandsHomepage.js
+++ utils/db/getBrandsHomepage.js
@@ -1,5 +1,8 @@
 import supabase from '@/utils/db/clients/public/supabase';
 
+/*Este endpoint é agora inútil, visto que a única diferença que tinha em relação ao getBrands era o limite de rows (9). Como tal, o getBrands
+encontra-se agora a aceitar uma variável que define o limite de pedidos. */
+
 const getBrandsHomepage = async () => {
     const { data } = await supabase
         .from('brands')

e0ce2a953ff4a2da6989136f14b240d9afb7d3b8
Margarida Bicho <margarida.bicho@protonmail.com>
2024-05-25 20:38:12 +0100

    sem linkedin e youtube com linkedin

--- components/logos/SocialMediaLogos.jsx
+++ components/logos/SocialMediaLogos.jsx
@@ -1,9 +1,8 @@
 import Image from "next/image";
 import FacebookLogo from "@/public/static/images/social_media/facebook_logo.svg";
 import InstagramLogo from "@/public/static/images/social_media/instagram_logo.svg";
-import TwitterLogo from "@/public/static/images/social_media/x_twitter_logo.svg";
-import YoutubeLogo from "@/public/static/images/social_media/youtube_logo.svg";
 import TiktokLogo from "@/public/static/images/social_media/tiktok_logo.svg"
+import { LinkedIn } from "@mui/icons-material";
 
 
 
@@ -12,9 +11,9 @@ export default function SocialMediaLogos() {
     <div className="flex flex-wrap justify-center">
       <Image src={FacebookLogo} alt="Logótipo do Facebook" width={20} height={20} styles={{width: 'auto', height: 'auto'}} className="mr-5"/>
       <Image src={InstagramLogo} alt="Logótipo do Instagram" width={20} height={20} styles={{width: 'auto', height: 'auto'}} className="mr-5"/>
-      <Image src={TwitterLogo} alt="Logótipo do X" width={18} height={18} styles={{width: 'auto', height: 'auto'}} className="mr-5"/>
-      <Image src={YoutubeLogo} alt="Logótipo do Youtube" width={20} height={14} styles={{width: 'auto', height: 'auto'}} className="mr-5"/>
+      <Image src={LinkedIn} alt="Logótipo do Linkedin" width={18} height={18} styles={{width: 'auto', height: 'auto'}} className="mr-5"/>
       <Image src={TiktokLogo} alt="Logótipo do TikTok" width={18} height={20} styles={{width: 'auto', height: 'auto'}} />
     </div>
+    //Margarida//
   );
 }

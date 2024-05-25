6829b9e2a0dc2ed3c3457d42442ce178c28525d1
Margarida Bicho <margarida.bicho@protonmail.com>
2024-05-25 21:06:28 +0100

    adicionados links para social media

--- components/logos/SocialMediaLogos.jsx
+++ components/logos/SocialMediaLogos.jsx
@@ -2,18 +2,18 @@ import Image from "next/image";
 import FacebookLogo from "@/public/static/images/social_media/facebook_logo.svg";
 import InstagramLogo from "@/public/static/images/social_media/instagram_logo.svg";
 import TiktokLogo from "@/public/static/images/social_media/tiktok_logo.svg"
-import { LinkedIn } from "@mui/icons-material";
+import { LinkedInLogo} from "@mui/icons-material";
 
 
 
 export default function SocialMediaLogos() {
   return (
     <div className="flex flex-wrap justify-center">
-      <Image src={FacebookLogo} alt="Logótipo do Facebook" width={20} height={20} styles={{width: 'auto', height: 'auto'}} className="mr-5"/>
-      <Image src={InstagramLogo} alt="Logótipo do Instagram" width={20} height={20} styles={{width: 'auto', height: 'auto'}} className="mr-5"/>
-      <Image src={LinkedIn} alt="Logótipo do Linkedin" width={18} height={18} styles={{width: 'auto', height: 'auto'}} className="mr-5"/>
-      <Image src={TiktokLogo} alt="Logótipo do TikTok" width={18} height={20} styles={{width: 'auto', height: 'auto'}} />
+      <a href="www.facebook.com"><Image src={FacebookLogo} alt="Logótipo do Facebook" width={20} height={20} styles={{width: 'auto', height: 'auto'}} className="mr-5"/></a>
+      <a href="www.instagram.com"><Image src={InstagramLogo} alt="Logótipo do Instagram" width={20} height={20} styles={{width: 'auto', height: 'auto'}} className="mr-5"/></a>
+      <a href="www.linkedin.com"><Image src={LinkedInLogo} alt="Logótipo do Linkedin" width={18} height={18} styles={{width: 'auto', height: 'auto'}} className="mr-5"/></a>
+      <a href="www.tiktok.com"><Image src={TiktokLogo} alt="Logótipo do TikTok" width={18} height={20} styles={{width: 'auto', height: 'auto'}} /></a>
     </div>
-    //Margarida//
+    //-----Margarida-----//
   );
 }

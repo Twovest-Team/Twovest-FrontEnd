efa72c0dec183da8c5dd1b3c968e4f6d56bd2313
Vasco Saraiva <vasco.saraivaua.pt>
2024-05-29 09:55:00 +0100

    Fix scroll

--- app/LandingPageMP/componentsMP/FeedbackSticky.jsx
+++ app/LandingPageMP/componentsMP/FeedbackSticky.jsx
@@ -1,11 +1,11 @@
 export default function FeedbackSticky () {
     return(
-        <div className="z-30 top-16 w-full h-[72px] px-[22px] sticky py-[17px] bg-black flex-col justify-center items-start gap-2.5 inline-flex">
+        <div className="z-30 top-[75px] w-full h-[72px] px-[22px] sticky py-[17px] bg-black flex-col justify-center items-start gap-2.5 inline-flex">
         <div className="w-[346px]">
             <p className="text-white leading-5 text-sm font-semibold">
                 Gostarias de dar feedback? 🤓 São apenas uns minutos e nós agradecemos!  
                 <a 
-                className="text-emerald-500 text-sm font-semibold pl-1"
+                className="text-emerald-500 text-sm font-semibold pl-1 underline"
                 href="https://forms.gle/5Y3qUDvL5JmMkyYd6">
                      Dar feedback.
                 </a>

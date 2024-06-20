23c538c57f8b0e5d8115c4f8201d41cf13085ddd
Jónatas Roque <jonatasroque@ua.pt>
2024-06-20 19:14:54 +0100

    design da pagina de requisitos de submissão de looks

--- app/gallery/submitLook/page.jsx
+++ app/gallery/submitLook/page.jsx
@@ -7,6 +7,11 @@ import NavigationTitle from "@/components/providers/NavigationTitle";
 import { useEffect, useState } from "react";
 import Button from "@/components/buttons/Button";
 import useAuth from "@/hooks/client-hooks/useAuth";
+import CheckroomOutlinedIcon from '@mui/icons-material/CheckroomOutlined';
+import PhotoCameraIcon from '@mui/icons-material/PhotoCamera';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import GavelIcon from '@mui/icons-material/Gavel';
+import BrushIcon from '@mui/icons-material/Brush';
 
 const Submit = () => {
 
@@ -28,57 +33,65 @@ const Submit = () => {
 
   return (
     <>
-      {currentUser && (
+      {currentUser && ( 
         <>
           <NavigationTitle titleText="Submissão de look" />
-          <div className="container mx-auto p-4 look overflow-hidden">
-            <div className="overlap relative mt-10">
-              <div className="overlap-group relative ">
-                <div className="ellipse overflow-hidden "></div>
-                <div className="label relative z-10 mt-24 mx-auto mb-48 requeirements">
-                  <h1 className="text-wrapper mb-10 mx-4 text_h6">
-                    Requisitos para submissão de Look
+          <div className="container mx-auto look overflow-hidden lg:w-[650px]">
+            <div className="">
+              <div className=" ">
+                <div className="">
+                  <h1 className="text_h6 mt-4 mb-8 text-center md:text-left">
+                    Requisitos para a submissão
                   </h1>
-                  <div className=" mb-28 subtitles mx-4">
-                    <ul>
-                      <li>
-                        <span className="span">
-                          Obrigatório preencher todos os campos que tenham
-                        </span>
-                        <span className="text-wrapper-11">*</span>
+                  <div className=" mb-10 subtitles">
+                    <ul className="">
+                    
+                      <li className="my-5 flex items-center"><div className="p-3.5 bg-primary_main rounded-[12px] mr-3"><GavelIcon className="text-white"/></div> <div>Obrigatório preencher todos os campos que tenham "<span className="text-primary_main font-semibold">*</span>"</div></li>
+                      
+                      <li className="my-5 flex items-center"><div className="p-3.5 bg-primary_main rounded-[12px] mr-3"><PhotoCameraIcon className="text-white"/></div><div>Tem de conter 1 foto</div></li>
+                      
+                      <li className="my-5 flex items-center"><div className="p-3.5 bg-primary_main rounded-[12px] mr-3"><BrushIcon className="text-white"/></div>Tem de conter 1 estilo associado</li>
+                      
+                      <li className="my-5 flex items-center">
+                      <div className="p-3.5 bg-primary_main rounded-[12px] mr-3"><CheckroomOutlinedIcon className="text-white"/></div>Uso de 2 ou mais artigos comprados na Twovest
                       </li>
-                      <li>Tem de conter 1 foto</li>
-                      <li>Tem de conter 1 estilo associado</li>
-                      <li>
-                        Uso de 2 ou mais peças de roupa comprada na Twovest
+                      
+                      <li className="my-5 flex items-center">
+                      <div className="p-3.5 bg-primary_main rounded-[12px] mr-3"><VisibilityIcon className="text-white"/></div>Só poderás submeter um novo look após a verificação da
+                        Twovest
                       </li>
-                      <li>
-                        Só poderás submeter um novo look após a verificação da
-                        Twovest ao teu look anterior.
-                      </li>
+                      
                     </ul>
-                    <span>
-                      Ver mais
-                      <br />
-                    </span>
                   </div>
 
+                  <div className="text_caption text-secondary">Podes prosseguir em: {timer} segundos</div>
+                  <div className="block md:flex mb-12 gap-2">
                   <Button
-                  href="submitLook/formLook"
+                  href="submitLook/formLook?gender=women"
                   width="100%"
-                  className="mx-auto"
+                  className="mx-auto my-2"
                   disabled={timer > 0}
-                  type='primary'
+                  type='black'
                   ariaLabel='Compreendi'>
-                    Compreendi ({timer})
+                    Submeter na Galeria Feminina
                   </Button>
-
+                  <Button
+                  href="submitLook/formLook?gender=men"
+                  width="100%"
+                  className="my-2 mx-auto"
+                  disabled={timer > 0}
+                  type='black'
+                  ariaLabel='Compreendi'>
+                    Submeter na Galeria Masculina
+                  </Button>
+                  </div>
+                  
                 </div>
               </div>
             </div>
           </div>
         </>
-      )}
+      )} 
     </>
   );
 };

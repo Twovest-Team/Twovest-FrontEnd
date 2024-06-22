37ebc731b86f33b5b13140483fd92184033282a9
Vasco Saraiva <vasco.saraivaua.pt>
2024-06-22 13:45:51 +0100

    Removed number 0 from upvotes on look card

--- components/buttons/icons/LookUpvoteButton.jsx
+++ components/buttons/icons/LookUpvoteButton.jsx
@@ -1,9 +1,10 @@
 //Icone de upvote, com e sem preenchimento
-import ForwardIcon from "@mui/icons-material/Forward";
 import ForwardOutlinedIcon from "@mui/icons-material/ForwardOutlined";
 import formatCompactNumbers from "@/utils/formatCompactNumbers";
 
 export default function LookUpvoteButton({ upvotes }) {
+
+  
   return (
     <div className="absolute top-3 right-3 hover:shadow-2xl text-white px-4 h-[40px] bg-black bg-opacity-70 hover:bg-opacity-100 transition-all duration-150 rounded-full justify-center items-center inline-flex gap-0.5">
       <ForwardOutlinedIcon
@@ -11,11 +12,14 @@ export default function LookUpvoteButton({ upvotes }) {
         ariaLabel="Botão de upvote"
         sx={{ fontSize: 24 }}
       />
-      <div className="text-center">
-        <h1 className="font-medium text-[16px]">
-          {formatCompactNumbers(upvotes)}
-        </h1>
-      </div>
+      {upvotes > 0 &&
+        <div className="text-center">
+          <h1 className="font-medium text-[16px]">
+            {formatCompactNumbers(upvotes)}
+          </h1>
+        </div>
+      }
+
     </div>
   );
 }

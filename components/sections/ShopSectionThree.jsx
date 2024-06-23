b208a137036f615244d7fcfe0227d32ba55dd4a1
jkapa17 <jpaixao@ua.pt>
2024-06-23 11:12:35 +0100

    Solved merge conflicts

--- components/sections/ShopSectionThree.jsx
+++ components/sections/ShopSectionThree.jsx
@@ -19,38 +19,28 @@ import applyPriceDiscount from "@/utils/applyPriceDiscount";
 const ShopSectionThree = ({ productsData, userData, couponData }) => {
   const { currentUser } = useAuth();
   const [cupaoVerificado, setCupaoVerificado] = useState("");
-  
+
   const products = useAppSelector((state) => state.cartProducts.products);
 
   useEffect(() => {
-      if(couponData)
-        {
-          const checkIfCouponIsValid = async () => {
-            try {
-              const coupons = await checkIfUserHasCoupon(currentUser.id, couponData.id_coupon);
-              setCupaoVerificado(coupons[0]);
-            } catch (error) {
-              console.error("Failed to fetch user coupons:", error);
-            }
-          };
-    
-          checkIfCouponIsValid();
-          console.log("Não esquecer de tirar 1 á quantity depois de dar sucesso")
+    if (couponData) {
+      const checkIfCouponIsValid = async () => {
+        try {
+          const coupons = await checkIfUserHasCoupon(
+            currentUser.id,
+            couponData.id_coupon
+          );
+          setCupaoVerificado(coupons[0]);
+        } catch (error) {
+          console.error("Failed to fetch user coupon:", error);
         }
+      };
 
-        else {
-          console.log("Não fazer nada")
-        }
+      checkIfCouponIsValid();
+    }
   }, [couponData]);
 
   const handlePurchase = async (produtos) => {
-
-    
-      
-    
-
-    
-    
     //Alterar o array de produtos de forma a calcular o preço total de todos os produtos.
     // O preço pode ser igual à média da primeira instancia do produto (com cup) com as outras instancias do produto (semp cup)
     // dessa forma, po stripe multiplica o preço pela quantidade, e dá o número certo
@@ -58,104 +48,102 @@ const ShopSectionThree = ({ productsData, userData, couponData }) => {
 
     let novosProdutos = [];
 
-    
-    if(cupaoVerificado) {
-      let couponBrandId = cupaoVerificado.coupons.coupons_has_brands[0].id_brand;
+    if (cupaoVerificado) {
+      let couponBrandId =
+        cupaoVerificado.coupons.coupons_has_brands[0].id_brand;
       let couponDiscount = cupaoVerificado.coupons.discount;
 
-    produtos.forEach(produto => {
-      let copiaProduto = JSON.parse(JSON.stringify(produto));
-      
+      produtos.forEach((produto) => {
+        let copiaProduto = JSON.parse(JSON.stringify(produto));
 
-      if(copiaProduto.offers.products.discount > 0)
-      {
-        if(copiaProduto.offers.products.brands.id == couponBrandId)
-        {
-
-       
-          if(copiaProduto.qty == 1) {
-            // O produto tem desconto e cupão mas é apenas 1
-            let discPrice = applyPriceDiscount(copiaProduto.offers.price, copiaProduto.offers.products.discount)
-            copiaProduto.offers.price = applyPriceDiscount(discPrice, couponDiscount);
-          }
-          if(copiaProduto.qty > 1)
-          {
-            // O produto tem desconto e cupão mas é mais que 1. O cupão só se aplica a 1 quantidade
-              let discPrice = applyPriceDiscount(copiaProduto.offers.price, copiaProduto.offers.products.discount)
-             
-                let novoProduto = JSON.parse(JSON.stringify(copiaProduto));
-                // Define o novo preço no objeto clonado
-                novoProduto.offers.price = discPrice;
-                novoProduto.qty = copiaProduto.qty - 1;
-                // Adiciona o objeto clonado ao array auxiliar
-                novosProdutos.push(novoProduto);
-
-                
-
-                //Aplica o desconto do cupão ao item e define a quantity para 1
-                copiaProduto.offers.price = applyPriceDiscount(discPrice, couponDiscount);
-                copiaProduto.qty = 1;
-          }
-          
-        }
-
-        else {
-          //Tem desconto mas não tem cupão
-          let discPrice = applyPriceDiscount(copiaProduto.offers.price, copiaProduto.offers.products.discount)
-          copiaProduto.offers.price = discPrice;
-        }
-      } if(copiaProduto.offers.products.discount == 0) {
-        if(copiaProduto.offers.products.brands.id == couponBrandId)
-          {
-            if(copiaProduto.qty == 1) {
-              // O produto tem cupão mas é apenas 1
-              copiaProduto.offers.price = applyPriceDiscount(copiaProduto.offers.price, couponDiscount);
+        if (copiaProduto.offers.products.discount > 0) {
+          if (copiaProduto.offers.products.brands.id == couponBrandId) {
+            if (copiaProduto.qty == 1) {
+              // O produto tem desconto e cupão mas é apenas 1
+              let discPrice = applyPriceDiscount(
+                copiaProduto.offers.price,
+                copiaProduto.offers.products.discount
+              );
+              copiaProduto.offers.price = applyPriceDiscount(
+                discPrice,
+                couponDiscount
+              );
             }
-            if(copiaProduto.qty > 1)
-              {
-                  // O produto tem cupão mas é mais que 1. O cupão só se aplica a 1 quantidade
+            if (copiaProduto.qty > 1) {
+              // O produto tem desconto e cupão mas é mais que 1. O cupão só se aplica a 1 quantidade
+              let discPrice = applyPriceDiscount(
+                copiaProduto.offers.price,
+                copiaProduto.offers.products.discount
+              );
 
-                  let novoProduto = JSON.parse(JSON.stringify(copiaProduto));
-                  // Define o novo preço no objeto clonado
-                  novoProduto.offers.price = copiaProduto.offers.price;
-                  novoProduto.qty = copiaProduto.qty - 1;
-                  // Adiciona o objeto clonado ao array auxiliar
-                  novosProdutos.push(novoProduto);
+              let novoProduto = JSON.parse(JSON.stringify(copiaProduto));
+              // Define o novo preço no objeto clonado
+              novoProduto.offers.price = discPrice;
+              novoProduto.qty = copiaProduto.qty - 1;
+              // Adiciona o objeto clonado ao array auxiliar
+              novosProdutos.push(novoProduto);
 
-                  //Aplica o desconto do cupão ao item e define a quantity para 1
-                  copiaProduto.offers.price = applyPriceDiscount(copiaProduto.offers.price, couponDiscount);
-                  copiaProduto.qty = 1;
-
-
-              }
-
-              
+              //Aplica o desconto do cupão ao item e define a quantity para 1
+              copiaProduto.offers.price = applyPriceDiscount(
+                discPrice,
+                couponDiscount
+              );
+              copiaProduto.qty = 1;
+            }
+          } else {
+            //Tem desconto mas não tem cupão
+            let discPrice = applyPriceDiscount(
+              copiaProduto.offers.price,
+              copiaProduto.offers.products.discount
+            );
+            copiaProduto.offers.price = discPrice;
           }
-      }
+        }
+        if (copiaProduto.offers.products.discount == 0) {
+          if (copiaProduto.offers.products.brands.id == couponBrandId) {
+            if (copiaProduto.qty == 1) {
+              // O produto tem cupão mas é apenas 1
+              copiaProduto.offers.price = applyPriceDiscount(
+                copiaProduto.offers.price,
+                couponDiscount
+              );
+            }
+            if (copiaProduto.qty > 1) {
+              // O produto tem cupão mas é mais que 1. O cupão só se aplica a 1 quantidade
 
-      novosProdutos.push(copiaProduto);
-    })
-    //Fim de if(cupaoVerificado)
-  }
+              let novoProduto = JSON.parse(JSON.stringify(copiaProduto));
+              // Define o novo preço no objeto clonado
+              novoProduto.offers.price = copiaProduto.offers.price;
+              novoProduto.qty = copiaProduto.qty - 1;
+              // Adiciona o objeto clonado ao array auxiliar
+              novosProdutos.push(novoProduto);
 
-  else {
+              //Aplica o desconto do cupão ao item e define a quantity para 1
+              copiaProduto.offers.price = applyPriceDiscount(
+                copiaProduto.offers.price,
+                couponDiscount
+              );
+              copiaProduto.qty = 1;
+            }
+          }
+        }
 
-    produtos.forEach(produto => {
-      let copiaProduto = JSON.parse(JSON.stringify(produto));
-    
-      if(copiaProduto.offers.products.discount > 0)
-      {
-            copiaProduto.offers.price = applyPriceDiscount(copiaProduto.offers.price, copiaProduto.offers.products.discount);
-      } 
+        novosProdutos.push(copiaProduto);
+      });
+    } else {
+      produtos.forEach((produto) => {
+        let copiaProduto = JSON.parse(JSON.stringify(produto));
 
-      novosProdutos.push(copiaProduto);
-    })
+        if (copiaProduto.offers.products.discount > 0) {
+          copiaProduto.offers.price = applyPriceDiscount(
+            copiaProduto.offers.price,
+            copiaProduto.offers.products.discount
+          );
+        }
 
-  }
-
-
-  console.log(novosProdutos)
-  
+        novosProdutos.push(copiaProduto);
+      });
+    }
 
     const purchaseData = novosProdutos.map((produto) => ({
       price_data: {
@@ -167,7 +155,7 @@ const ShopSectionThree = ({ productsData, userData, couponData }) => {
             produto.offers.colors.name,
           images: [getStorageImage(produto.offers.products.images[0].url)],
         },
-        unit_amount: Math.round(produto.offers.price * 100)
+        unit_amount: Math.round(produto.offers.price * 100),
       },
       quantity: produto.qty,
     }));
@@ -184,46 +172,84 @@ const ShopSectionThree = ({ productsData, userData, couponData }) => {
     } catch (error) {
       // Handle errors
       console.error("Error during purchase:", error);
-    } 
+    }
   };
 
   return (
     <div className="container">
-      <h1
-        className="flex gap-2 items-center font-semibold text_h6"
-      >
+      <h1 className="flex gap-2 items-center font-semibold text_h6">
         Escolhe um método de pagamento
       </h1>
 
       <div className="flex flex-col gap-4 mt-4">
-
-
-        <Button justify="start" onClick={() => handlePurchase(productsData)} type={'grey'} ariaLabel='Pagar com Cartão De Crédito' width='100%'>
-          <Image className="mr-2" src={MastercardLogo} alt="Logótipo da Mastercard" width={30} height={18} styles={{ width: 'auto', height: 'auto' }} />
+        <Button
+          justify="start"
+          onClick={() => handlePurchase(productsData)}
+          type={"grey"}
+          ariaLabel="Pagar com Cartão De Crédito"
+          width="100%"
+        >
+          <Image
+            className="mr-2"
+            src={MastercardLogo}
+            alt="Logótipo da Mastercard"
+            width={30}
+            height={18}
+            styles={{ width: "auto", height: "auto" }}
+          />
           Cartão De Crédito
         </Button>
 
-
-
-        <Button justify="start" onClick={() => handlePurchase(productsData)} type={'grey'} ariaLabel='Pagar com MBWay' width='100%'>
-          <Image className="mr-2" src={MBWayLogo} alt="Logótipo do MBWay" width={30} height={19}/>
+        <Button
+          justify="start"
+          onClick={() => handlePurchase(productsData)}
+          type={"grey"}
+          ariaLabel="Pagar com MBWay"
+          width="100%"
+        >
+          <Image
+            className="mr-2"
+            src={MBWayLogo}
+            alt="Logótipo do MBWay"
+            width={30}
+            height={19}
+          />
           MBWay
         </Button>
 
-
-
-        <Button justify="start" onClick={() => handlePurchase(productsData)} type={'grey'} ariaLabel='Pagar com GPay' width='full'>
-          <Image className="mr-2" src={GooglePayLogo} alt="Logótipo do GooglePay" width={30} height={17}/>
+        <Button
+          justify="start"
+          onClick={() => handlePurchase(productsData)}
+          type={"grey"}
+          ariaLabel="Pagar com GPay"
+          width="full"
+        >
+          <Image
+            className="mr-2"
+            src={GooglePayLogo}
+            alt="Logótipo do GooglePay"
+            width={30}
+            height={17}
+          />
           GPay
         </Button>
 
-
-
-        <Button justify="start" onClick={() => handlePurchase(productsData)} type={'grey'} ariaLabel='Pagar com Paypal' width='full'>
-          <Image className="mr-2" src={PaypalLogo} alt="Logótipo do GooglePay" width={30} height={17}/>
+        <Button
+          justify="start"
+          onClick={() => handlePurchase(productsData)}
+          type={"grey"}
+          ariaLabel="Pagar com Paypal"
+          width="full"
+        >
+          <Image
+            className="mr-2"
+            src={PaypalLogo}
+            alt="Logótipo do GooglePay"
+            width={30}
+            height={17}
+          />
           Paypal
         </Button>
-
       </div>
     </div>
   );
